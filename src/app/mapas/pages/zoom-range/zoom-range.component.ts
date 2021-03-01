11e61335a04a81d7d7fcdb3b9d7fa937844559fc
neryad <dayern.gomez@hotmail.com>
2021-03-01 18:43:03 -0400

    init zoomPage

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,16 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-zoom-range',
   templateUrl: './zoom-range.component.html',
   styles: [
-  ]
+    `
+      .mapa-container {
+        width: 100%;
+        height: 100%;
+      }
+      .row {
+        background-color: white;
+        position: fixed;
+        bottom: 50px;
+        left: 50px;
+        padding: 10px;
+        z-index: 999;
+        border-radius: 5px;
+      }
+    `,
+  ],
 })
 export class ZoomRangeComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    var map = new mapboxgl.Map({
+      container: 'mapa',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-69.93750922045666, 18.470937424234418],
+      zoom: 18,
+    });
   }
-
 }
